a14de346a92fc1fb6f517f8cec4684b9bca78aa4
foodisgod <69075108+foodisgod@users.noreply.github.com>
2022-08-13 18:11:59 +1000

    Add files via upload

---
+++ Hub.lua
@@ -0,0 +1,31 @@
+
+repeat wait() until game:IsLoaded()
+local place = game.PlaceId
+if place == 9964090091 then
+    print("Loading Hacker Simulator GUI")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgod/Emilys-Script/main/Hacker%20Simulator.lua"))()  -- https://www.roblox.com/games/9964090091/
+
+    elseif place == 9190926538 then
+    print("Loading Toy Simulator GUI")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgod/Emilys-Script/main/Toy%20Fighters.lua"))() --https://www.roblox.com/games/9190926538/
+
+    elseif place == 9157870347 then
+    print("Loading Pet Adventures Simulator! GUI")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgod/Emilys-Script/main/Pet%20Adventure%20Simulator.lua"))() --https://www.roblox.com/games/9157870347/
+    
+    elseif place == 9396463375 then
+    print("Loading Mining Empire GUI")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgod/Emilys-Script/main/Mining%20Empire.lua"))() --https://www.roblox.com/games/9396463375/
+    
+    elseif place == 9006256958 then
+    print("Loading Weed Wacking Simulator GUI")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgod/Emilys-Script/main/Weed%20Wacking%20Simulator.lua"))() --https://www.roblox.com/games/9006256958/
+    
+    elseif place == 9913434056 then
+    print("Loading Milkshake Simulator! GUI")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgod/Emilys-Script/main/Milkshake%20Simulator.lua"))() --https://www.roblox.com/games/9913434056/
+    
+    elseif place == 9577112413 then
+    print("Loading Dreaming Simulator 2 GUI")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/foodisgod/Emilys-Script/main/Dreaming%20Simulator%202.lua"))() --https://www.roblox.com/games/9577112413/
+end
